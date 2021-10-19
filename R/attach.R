dbc78873db79c143ac68e940ffe6bba9d526f4f2
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-10-19 09:52:47 +0200

    add retirement notice

---
+++ R/attach.R
@@ -0,0 +1,7 @@
+.onAttach <- function(...) {
+  packageStartupMessage(
+    "This package is no longer being maintained and will be archived on CRAN.
+    All its functionality and development has now moved to `{ggstatsplot}` package.
+    Please see: https://indrajeetpatil.github.io/ggstatsplot/"
+  )
+}
